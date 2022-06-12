193893ce0af3a96ce1301a0ab904bf32ebd7f063
Felipe Souza De Lima <99934674+Felipe-Souza-De-Lima@users.noreply.github.com>
2022-06-12 10:39:05 -0400

    Create index.js
    trabalhando com javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+var nome = "Felipe Souza De Lima";
+
+var idade = 19;
+
+var anoAtual = 2022;
+
+var resultado = anoAtual - idade;
+
+console.log(resultado);
+
+console.log("Estou mexendo no console.log");
+
+var quartaFeira = true;
+
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
+
+
+
+
+
+
+
+
