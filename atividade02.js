447251f289d26a9e911aaba95cfc5a9c4bd52ab6
Felipe Souza De Lima <99934674+Felipe-Souza-De-Lima@users.noreply.github.com>
2022-06-13 10:34:19 -0400

    atividade 2  - vai na web

---
+++ atividade02.js
@@ -0,0 +1,28 @@
+var maiorDeIdade = 18;
+
+if (maiorDeIdade >= 18) {
+    console.log("Você está apto a tira a CNH, meus parabéns!");
+} else {
+    console.log("Sinto muito, espere mais alguns anos, você é de menor!");
+}
+
+var deMaior = true;
+var verificarSerHumano = true;
+
+if (deMaior == verificarSerHumano) {
+    console.log("Você pode apostar em jogos, aproveite!");
+} else {
+    console.log("Infelizmente não será possível você apostar.")
+}
+
+var nasceuEmJaneiro = false;
+var nasceuEmDezembro = true;
+
+if (nasceuEmJaneiro || nasceuEmDezembro) {
+    console.log("Parabéns pelo seu mês de aniversário");
+
+} else {
+    console.log("Ops! Você não nasceu no mês de janeiro, nem em dezembro, sinto muito, não é o seu aniversário.");
+}
+
+
